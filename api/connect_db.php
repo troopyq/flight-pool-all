638136e58254db81b318028b7d07192644cdbf52
Dima Nemkov <nemkov03@mail.ru>
2021-01-20 13:08:40 +0500

    add database

--- api/connect_db.php
+++ api/connect_db.php
@@ -1,8 +1,8 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'root', 'test');
+$db = mysqli_connect('localhost', 'root', 'root', 'a0490169_dima');
 
 function db(){
-  $db = mysqli_connect('localhost', 'root', 'root', 'test');
+  $db = mysqli_connect('localhost', 'root', 'root', 'a0490169_dima');
   return $db;
 }
